2d81a7e5cc947d0430b14f47ef0b9b1bf69058f7
Eduardo Valenzuela <evalenzuela@aurasix.com>
2018-08-11 11:44:27 -0500

    Adding initial cars views

---
+++ src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Welcome to My Garage</h1>
+</template>
---
+++ src/components/cars/List.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Maker</th>
+        <th scope="col">Model</th>
+        <th scope="col">Year</th>
+        <th scope="col">Cost</th>
+        <th scope="col">Status</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Honda</td>
+        <td>Civic</td>
+        <td>1992</td>
+        <td>USD 2,000</td>
+        <td>Used</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Honda</td>
+        <td>CRD</td>
+        <td>2018</td>
+        <td>USD 27,000</td>
+        <td>New</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Kia</td>
+        <td>Cerato</td>
+        <td>2012</td>
+        <td>USD 18,000</td>
+        <td>Used</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
